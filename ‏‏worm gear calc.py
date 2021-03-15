a46f3141788a3a39221a017b1a36f39e0bbc66a6
IzukiSama <67558536+IzukiSama@users.noreply.github.com>
2021-03-15 18:48:12 +0200

    Add files via upload

---
+++ ‏‏worm gear calc.py
@@ -0,0 +1,12 @@
+#diameter=pitch*number of teeth/pi
+
+import math
+
+pitch=float(input("what is the pitch: "))
+teeth=float(input("what is the teeth number: "))
+
+print("diameter is: ",pitch*teeth/math.pi)
+
+temp=input("enter to close")
+
+
